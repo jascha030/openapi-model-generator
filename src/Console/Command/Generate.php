d4fb4a2ae741020a96a562b93da0a66469952a50
Jascha030 <contact@jaschavanaalst.nl>
2024-06-18 11:49:00 +0200

    ✨ Got high, wrote a foreach loop...

--- src/Console/Command/Generate.php
+++ src/Console/Command/Generate.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace Jascha030\OpenApiModelGenerator\Console\Command;
 
+use Jascha030\OpenApiModelGenerator\ClassGenerator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+
+use function getcwd;
+use function is_dir;
 
 #[AsCommand(
     name: 'generate',
-    description: 'Generate Serializable Models from OpenAPI/Swagger documentation.'
+    description: 'Generate Serializable Models from OpenAPI/Swagger documentation.',
 )]
 class Generate extends Command
 {
@@ -25,13 +30,25 @@ class Generate extends Command
     {
         $this
             ->addArgument('input-file', InputArgument::REQUIRED, 'path to swagger file.')
-            ->addArgument('output-dir', InputArgument::REQUIRED, 'path to output directory.');
+            ->addOption('namespace', 'n', InputArgument::REQUIRED, 'namespace for generated classes.')
+            ->addOption('output', 'o', InputArgument::OPTIONAL, 'path to output directory.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $inputPath = $input->getArgument('input-file');
-        $outputPath = $input->getArgument('outpur-dir');
+        $outputPath = $input->getOption('output') ?? getcwd() . '/Model';
+        $namespace = $input->getOption('namespace');
+
+        if (!is_dir($outputPath) && !mkdir($outputPath, 0777, true) && !is_dir($outputPath)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $outputPath));
+        }
+
+        $iter = new ClassGenerator($inputPath, $namespace);
+
+        foreach ($iter as $class) {
+            file_put_contents($outputPath . '/' . $class->getName() . '.php', $class->generate());
+        }
 
         return Command::SUCCESS;
     }
