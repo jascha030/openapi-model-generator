aa015a85a4a3d2eb4d078e494a229b7333c767c1
Jascha030 <contact@jaschavanaalst.nl>
2024-06-21 12:34:37 +0200

    🎨 php-cs-fixer cache dir.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,7 +5,32 @@ declare(strict_types=1);
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
+/**
+ * Cache dir and file location.
+ */
+$cacheDirectory = __DIR__ . '/.var/cache';
+$cacheFile      = "{$cacheDirectory}/.php-cs-fixer.cache";
+
+/**
+ * Create a .cache dir if not already present.
+ */
+if (
+    ! file_exists($cacheDirectory)
+    && ! mkdir($cacheDirectory, 0700, true)
+    && ! is_dir($cacheDirectory)
+) {
+    throw new RuntimeException(
+        sprintf('Directory "%s" was not created', $cacheDirectory)
+    );
+}
+
 return (new Config())
+    ->setRiskyAllowed(true)
+    ->setCacheFile($cacheFile)
+    ->setRules([
+        '@PER-CS'       => true,
+        '@PER-CS:risky' => true,
+    ])
     ->setFinder(
         Finder::create()
             ->in(__DIR__)
@@ -14,8 +39,4 @@ return (new Config())
                 'tools',
                 '.var',
             ])
-    )
-    ->setRules([
-        '@PER-CS'       => true,
-        '@PER-CS:risky' => true,
-    ]);
+    );
